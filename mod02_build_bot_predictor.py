fb167dfcc9b330af3ead0ea37df47dfdb3c70c31
yxt24001 <yxt24001@uconn.edu>
2026-09-18 01:27:49 +0000

    Homework 2

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        learning_rate=0.5,
+        n_estimators=500,
+        max_depth=1000,
+        subsample=2,
+        min_samples_leaf=5,
         random_state=seed
     )
     model.fit(X, y)
